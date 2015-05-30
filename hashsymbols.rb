8e462d880e6a16f3ad0fcbbb7d16999ba0181d8f
dseuphoria <jasmine@jemworkz.com>
2015-05-30 09:12:30 -0400

    my first commit

---
+++ hashsymbols.rb
@@ -0,0 +1,18 @@
+bridal_party_01 = {"name" => "Camara", "role" => "Maid of Honor"}
+bridal_party_02 = {"name" => "Tianna", "role" => "Matron of Honor"}
+bridal_party_03 = {"name" => "Malika", "role" => "Bridesmaid"}
+bridal_party_04 = {"name" => "Joi-Arielle", role => "Bridesmaid"}
+
+
+bridal_party_01 { :name => "Camara", :role => "Maid of Honor"}
+
+
+bridal_party_01 = { name: "Camara", role: "Maid of Honor"}
+bridal_party_02 = { name: "Tianna", role: "Matron of Honor"}
+bridal_party_03 = { name: "Malika", role: "Bridesmaid"}
+bridal_party_04 = { name: "Joi-Arielle", role: "Bridesmaid"}
+
+
+moh = bridal_party_01[:name]
+moh_role = bridal_party_01[:role]
+puts "#{moh} is going to be my #{moh_role}"
